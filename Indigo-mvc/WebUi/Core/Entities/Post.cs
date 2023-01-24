538f88687b8e5953f6a9d5705afed3ba6b0e47f5
OzlamHasanova <7qk9bme@code.edu.az>
2023-01-24 07:21:05 +0400

    first

---
+++ Indigo-mvc/WebUi/Core/Entities/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string image { get; set; }
+        public string title { get; set; }
+        public string desc { get; set; }
+
+    }
+}
